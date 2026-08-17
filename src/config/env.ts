80dd13105d622838efbe3aac4e5bc6243a14eedb
digbanmije18@gmail.com <digbanmije18@gmail.com>
2026-08-17 06:59:45 +0100

    Feat: project scaffold

---
+++ src/config/env.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+export const required = (key: string) => {
+  const value = process.env[key]
+
+  if (!value) {
+    throw new Error(`Missing env variables ${key}`)
+  }
+
+  return value
+}
+
+export const env = {
+  port: Number(process.env.PORT),
+  databaseUrl: required('DATABASE_URL'),
+  paystackSecretKey: required('PAYSTACK_SECRET_KEY'),
+  paystackPublicKey: required('PAYSTACK_PUBLIC_KEY'),
+  coursePriceNaira: {
+    // "Web Development": Number(process.env.)
+  }
+}
