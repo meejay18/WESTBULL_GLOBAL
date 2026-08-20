1de08bf3f78eb433d1c8dec713d6b0b94e0c9269
meejay18 <digbanmije18@gmail.com>
2026-08-20 15:38:53 +0100

    Port binding

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,25 @@
-import { app } from './app'
-import { env } from './config/env'
-import { prisma } from './config/prisma'
-import { logger } from './utils/logger'
+import { app } from "./app";
+import { env } from "./config/env";
+import { prisma } from "./config/prisma";
+import { logger } from "./utils/logger";
 
 export const startServer = async () => {
   try {
-    await prisma.$connect()
+    await prisma.$connect();
 
-    logger.info('Database connected successfully')
+    logger.info("Database connected successfully");
 
-    app.listen(env.port, '127.0.0.1', () => {
-      logger.info(`App started on port ${env.port}`)
-      logger.info(`Health check: http://localhost:${env.port}/health`)
-      logger.info(`Webhook path to register on Paystack dashboard: /webhooks/paystack`)
-    })
+    app.listen(env.port, "0.0.0.0", () => {
+      logger.info(`App started on port ${env.port}`);
+      logger.info(`Health check: http://localhost:${env.port}/health`);
+      logger.info(
+        `Webhook path to register on Paystack dashboard: /webhooks/paystack`,
+      );
+    });
   } catch (error) {
-    logger.error(error, 'Failed to connect to database')
-    process.exit(1)
+    logger.error(error, "Failed to connect to database");
+    process.exit(1);
   }
-}
+};
 
-startServer()
+startServer();
