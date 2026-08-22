97bcec4052df20ad2b196df5d777e9e21911f3b5
digbanmije18@gmail.com <digbanmije18@gmail.com>
2026-08-22 22:08:27 +0100

    Feat:paymant and webhook endpoint added

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -5,7 +5,7 @@ import { paystackWebhookController } from '../controllers/webhook.controller'
 const router = Router()
 
 router.post(
-  '/paystack',
+  '/',
   express.raw({ type: 'application/json' }),
   paystackWebhookController,
 )
